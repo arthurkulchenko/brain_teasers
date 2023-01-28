8674d06ce5af303db1e38d77bfbe0579126b5197
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-01-28 23:31:31 +0300

    add 8th teser

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod type_conversion_3;
 mod byte_sized_chunks_4;
 mod how_long_is_string_5;
 mod please_reboot_the_univers_6;
+mod there_and_back_again_7;
+mod walks_like_a_duck_quacks_like_a_duck_8;
 
 fn main() {
   please_reboot_the_univers_6::call();
