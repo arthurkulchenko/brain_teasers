506230e7801b7415d375da496cc26e1e67ce8de4
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2023-01-28 23:46:08 +0300

    add 9th teaser

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod how_long_is_string_5;
 mod please_reboot_the_univers_6;
 mod there_and_back_again_7;
 mod walks_like_a_duck_quacks_like_a_duck_8;
+mod out_of_order_9;
 
 fn main() {
   please_reboot_the_univers_6::call();
